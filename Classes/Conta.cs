09b8e82364b98e0d5cf6c821981c6d27fd0fb345
felipefs <felipe.fs.vr@gmail.com>
2021-03-01 20:02:33 -0300

    Implementado Sacar, Depositar e Transferir

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -1,14 +1,17 @@
+using System;
+
 namespace DIO.Bank
 {
     public class Conta
     {
 
-        private TipoConta TipoConta{get;set;}
+        private TipoConta TipoConta { get; set; }
         private double Saldo { get; set; }
-        private double Credito { get; set; } 
+        private double Credito { get; set; }
         private string Nome { get; set; }
 
-        public Conta(TipoConta _tipoconta, double _saldo, double  _credito, string _nome){
+        public Conta(TipoConta _tipoconta, double _saldo, double _credito, string _nome)
+        {
             this.TipoConta = _tipoconta;
             this.Saldo = _saldo;
             this.Credito = _credito;
@@ -16,6 +19,42 @@ namespace DIO.Bank
 
         }
 
-        
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < this.Credito * -1)
+            {
+                Console.WriteLine("Saldo Insufuciente.");
+                return false;
+
+            }
+
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+
+        }
+
+        public void Depositar(double valorDeposito){
+            this.Saldo += valorDeposito;
+            Console.WriteLine("Saldo atual de conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino){
+            if(this.Sacar(valorTransferencia)){
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno+= "TipoConta: " + this.TipoConta + " |";
+            retorno+= "Nome: " + this.Nome + " |";
+            retorno+= "Saldo: " + this.Saldo + " |";
+            retorno+= "Credito: " + this.Credito;
+
+            return retorno;
+        }
+
     }
 }
